eeaef52ba3d80c684c62cfccc98dfbab53fa315c
Robin Rana <robin4500bd@gmail.com>
2024-04-24 15:30:57 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref, reactive } from "vue";
+
+// Person state
+const person = reactive({
+  name: "Robin Rana",
+  age: 20,
+});
+
+// // Person state
+// const person = ref({
+//   name: "Robin Rana",
+//   age: 20,
+// });
+
+// Increase person age
+const ageIncrement = () => {
+  person.age++;
+};
+
+// Decrement person age
+const ageDecrement = () => {
+  person.age--;
+};
+
+// Person computed logic
+const isQualified = computed(() => {
+  return person.age > 18 ? "Qualified" : "Not Qualified";
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>Name is {{ person.name }}</h2>
+  <h2>Age is {{ person.age }}</h2>
+  <h2>
+    You are <span>{{ isQualified }}</span>
+  </h2>
+  <button @click="ageIncrement">Increase Age</button>
+  <button @click="ageDecrement">Decrement Age</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
